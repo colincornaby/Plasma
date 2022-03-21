d06262984b2e1a8f45917e75264a9fc4fa1f9c9b
Colin Cornaby <colin.cornaby@mac.com>
2022-03-20 20:14:20 -0700

    Initial work to resync Metal work on master
    Does not yet link - async sockets missing implementation

--- Sources/Plasma/FeatureLib/inc/pfAllCreatables.h
+++ Sources/Plasma/FeatureLib/inc/pfAllCreatables.h
@@ -59,6 +59,10 @@ You can contact Cyan Worlds, Inc. by email legal@cyan.com
 
 #ifdef PLASMA_PIPELINE_GL
     #include "pfGLPipeline/pfGLPipelineCreatable.h"
+#endif"
+
+#ifdef PLASMA_PIPELINE_METAL
+    #include "pfMetalPipeline/pfMetalPipelineCreatable.h"
 #endif
 
 #include "pfJournalBook/pfJournalBookCreatable.h"
