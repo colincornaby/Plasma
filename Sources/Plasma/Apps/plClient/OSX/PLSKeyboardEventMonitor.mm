9ad65453c5013e48f34150129c4d2bb57a2f4ec3
Colin Cornaby <colin.cornaby@mac.com>
2022-04-24 22:09:06 -0700

    Fixing Mac keyboard input
    For now putting Mac keyboards directly into Plasma tables. HID keys don’t work with keybindings right now. This approach could change once the HID/keybinding conflict is worked out.

--- Sources/Plasma/Apps/plClient/OSX/PLSKeyboardEventMonitor.mm
+++ Sources/Plasma/Apps/plClient/OSX/PLSKeyboardEventMonitor.mm
@@ -45,6 +45,7 @@ You can contact Cyan Worlds, Inc. by email legal@cyan.com
 #include "plClient/plClientLoader.h"
 #include "plInputCore/plInputManager.h"
 #import "PLSKeyboardEventMonitor.h"
+#include <Carbon/Carbon.h>
 
 /*
  This class implements a Cocoa keyboard tap for Plasma.
@@ -121,43 +122,18 @@ NSEventMaskFlagsChanged;
         return NO;
     }
     
-    //Cocoa keycodes use the ids from the original Mac.
-    //Note that Cocoa will not return keycodes for modifier keys like shift.
-    //We have to check the modifier flags for that.
-    const unsigned short KEYCODE_MAC_TO_HID[0x7F] = {
-         4, 22,  7,  9, 11, 10, 29, 27, 06, 25, 00, 05, 20, 26,  8, 21,
-        //0x10
-        28, 23, 30, 31, 32, 33, 35, 34, 46, 38, 36, 45, 37, 39, 48, 18,
-        //0x20
-        24, 47, 12, 19,158, 15, 13, 52, 14, 51, 49, 54, 56, 17, 16, 55,
-        //0x30
-        43, 44, 53, 42, 41, 0, 0, 0, 225, 57, 226,
-        //0x3B
-        224, 0, 0, 0, 0,
-        //0x40
-        00, 99, 00, 85, 00, 87, 00, 00, 00, 00, 00, 84, 158, 0, 86, 00,
-        //0x50
-        00, 00, 98, 89, 90, 91, 92, 93, 94, 95, 00, 96, 97,
-        //0x5D
-        0, 0, 0,
-        //0x60
-        62, 63, 64, 60, 65, 66, 00, 68, 00, 00, 00, 00, 00, 67, 00, 69,
-        //0x70
-        00, 00, 00, 74, 75, 76, 61, 77, 59, 78, 58, 80, 79, 81, 82
-    };
-    
     BOOL down = event.type == NSEventTypeKeyDown;
     
     unsigned short keycode = [event keyCode];
     plKeyDef convertedKey = (plKeyDef)KEYCODE_MAC_TO_HID[keycode];
     //if it's a shift key event only way to derive up or down is through presence in the modifier flag
-    if(convertedKey == KEY_SHIFT) {
+    if(keycode == kVK_Shift) {
         down = (event.modifierFlags & NSEventModifierFlagShift) != 0;
     }
-    if(convertedKey == KEY_ALT) {
+    if(keycode == kVK_Option) {
         down = (event.modifierFlags & NSEventModifierFlagOption) != 0;
     }
-    if(convertedKey == KEY_CTRL) {
+    if(keycode == kVK_Control) {
         down = (event.modifierFlags & NSEventModifierFlagControl) != 0;
     }
     
@@ -169,17 +145,19 @@ NSEventMaskFlagsChanged;
      as "function keys". So we want to not trap events that are function key events, but we do want to trap the arrow keys.
      */
     //Edit 2: We also want to catch the function key modifier but not the actual function keys
-    if (!(convertedKey == KEY_LEFT || convertedKey == KEY_RIGHT || convertedKey == KEY_UP || convertedKey == KEY_DOWN || (convertedKey >= 58 && convertedKey <= 69)) &&  modifierFlags & NSEventModifierFlagFunction) {
+    if (!(keycode == kVK_LeftArrow || keycode == kVK_RightArrow || keycode == kVK_UpArrow || keycode == kVK_DownArrow || (convertedKey >= 99 && convertedKey <= 113)) &&  modifierFlags & NSEventModifierFlagFunction) {
         return NO;
     }
     
     @synchronized (self.view.layer) {
-        if (modifierFlags & NSEventModifierFlagFunction) {
-            NSLog(@"Key event %@, Plasma key is %i, is down %i, repeat is %i", event, convertedKey, down, event.ARepeat);
-            self.inputManager->HandleKeyEvent(convertedKey, down, event.ARepeat);
-        } else {
-            char character = [event.characters cStringUsingEncoding:NSUTF8StringEncoding][0];
-            self.inputManager->HandleKeyEvent(convertedKey, down, event.ARepeat, character);
+        self.inputManager->HandleKeyEvent((plKeyDef)keycode, down, event.type == NSEventTypeFlagsChanged ? false : event.ARepeat);
+        if (!(modifierFlags & NSEventModifierFlagFunction) && down) {
+            if(event.type != NSEventTypeFlagsChanged && event.characters.length > 0) {
+                char character = [event.characters cStringUsingEncoding:NSUTF8StringEncoding][0];
+                if(!std::iscntrl(character)) {
+                    self.inputManager->HandleKeyEvent((plKeyDef)keycode, down, event.type == NSEventTypeFlagsChanged ? false : event.ARepeat, character);
+                }
+            }
         }
     }
     return YES;
