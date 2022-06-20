8782cdfc7a58c644ec4778f47237dc286ffeb734
Colin Cornaby <colin.cornaby@mac.com>
2022-06-19 21:33:22 -0700

    Switching to backing rect for output resolution

--- Sources/Plasma/Apps/plClient/OSX/PLSView.mm
+++ Sources/Plasma/Apps/plClient/OSX/PLSView.mm
@@ -230,9 +230,7 @@ You can contact Cyan Worlds, Inc. by email legal@cyan.com
 
 - (void)resizeDrawable:(CGFloat)scaleFactor
 {
-    CGSize newSize = self.bounds.size;
-    newSize.width *= scaleFactor;
-    newSize.height *= scaleFactor;
+    CGSize newSize = [self convertRectToBacking:self.bounds].size;
 
     if(newSize.width <= 0 || newSize.width <= 0)
     {
